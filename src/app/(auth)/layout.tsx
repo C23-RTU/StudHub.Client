9dcb202c4458019ae5728025ef13d1df6f639463
TankistPro <nakol.nayk@mail.ru>
2025-01-06 23:47:08 +0300

    Добавлена панель навигации, настройка стилей

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,5 +1,12 @@
 import type { PropsWithChildren } from 'react';
 
+import { MenuNavigation } from '@/components/MenuNavigation/MenuNavigation';
+
 export default function Layout({ children }: PropsWithChildren<unknown>) {
-    return <div className="h-full">{children}</div>;
+    return (
+        <div className="h-[calc(100svh-theme(height.menuNavigation))]">
+            {children}
+            <MenuNavigation />
+        </div>
+    );
 }
