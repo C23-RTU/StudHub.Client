32ba372df5e50108cdd594e65ee5e061892e1df9
Justifall <84130430+Angst04@users.noreply.github.com>
2025-01-08 00:40:52 +0300

    [SN-9] Добавлена страница регистрации

--- src/app/(public)/login/form.tsx
+++ src/app/(public)/login/form.tsx
@@ -1,9 +1,15 @@
 "use client";
 
+import { useState } from "react";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
+import { Eye, EyeClosed } from 'lucide-react';
+
 export default function LoginForm() {
+    const [showPassword, setShowPassword] = useState(false);
+
     async function handleSubmit(event: React.FormEvent) {
         event.preventDefault();
 
@@ -12,7 +18,16 @@ export default function LoginForm() {
     return (
         <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <Input placeholder="Почта" type="email" />
-            <Input placeholder="Пароль" type="password" />
+            <div className="relative">
+                <Input name='password' placeholder="Пароль" type={showPassword ? "text" : "password"} />
+                {showPassword ? (
+                    <Eye className="absolute top-1/2 right-2 -translate-y-1/2 cursor-pointer" size={20} 
+                        onClick={() => setShowPassword(!showPassword)} />
+                ) : (
+                    <EyeClosed className="absolute top-1/2 right-2 -translate-y-1/2 cursor-pointer" size={20} 
+                        onClick={() => setShowPassword(!showPassword)}/>
+                )}
+            </div>
             <Button type="submit">Войти</Button>
         </form>
     );
