7f1bbe948c528cd3d790b81551e24ecd39f0e5dc
TankistPro <nakol.nayk@mail.ru>
2025-01-11 00:37:57 +0300

    Добавлены страницы PublicPage

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from '@/components/ui/button';
-
-export default function Home() {
-    return (
-        <div className="flex justify-center items-center h-[100dvh]">
-            <Button>Нажми на меня</Button>
-        </div>
-    );
-}
--- src/lib/config/routes.config.ts
+++ src/lib/config/routes.config.ts
@@ -1,9 +1,15 @@
 class AuthPage {
-    HOME = '/home';
+    HOME = '/';
     EVENTS = '/events';
     PROFILE = '/profile';
     CLUBS = '/clubs';
     CALENDAR = '/calendar';
 }
 
+class PublicPage {
+    LOGIN = '/login';
+    REGISTRATION = '/registration';
+}
+
 export const AUTH_PAGE = new AuthPage();
+export const PUBLIC_PAGE = new PublicPage();
