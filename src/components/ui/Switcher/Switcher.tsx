f641410ec8a2e96bb7865175f36f7e6c44e1f32e
TankistPro <nakol.nayk@mail.ru>
2025-01-14 20:14:26 +0300

    Добавлен Switcher

---
+++ src/components/ui/Switcher/Switcher.tsx
@@ -0,0 +1,12 @@
+export function Switcher() {
+    return (
+        <div className="flex justify-between w-full bg-white h-[40px] rounded-lg">
+            <div className="flex-1 flex items-center justify-center text-black">
+                <p>Вход</p>
+            </div>
+            <div>
+                <p>Регистарция</p>
+            </div>
+        </div>
+    );
+}
