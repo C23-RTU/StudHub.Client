5af4c0ef14e06a120772dfe523964cb2231e5f5c
TankistPro <nakol.nayk@mail.ru>
2025-01-18 15:21:40 +0300

    Отключена авторизация в DEV режиме, исправил регулярное выражение

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,13 +2,18 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 import { PUBLIC_PAGE } from './lib/config/routes.config';
+import { isProduction } from './lib/isProdation.env';
 import { authorizationProtect } from './middleware/authorization-protect.middleware';
 import { loginProtect } from './middleware/login-protect.middleware';
 
 export async function middleware(request: NextRequest) {
     const url = new URL(request.url);
     const pathname = url.pathname;
-    console.log(request.cookies);
+
+    if (!isProduction) {
+        return NextResponse.next();
+    }
+
     if (!pathname.includes(PUBLIC_PAGE.AUTH)) {
         return authorizationProtect(request);
     }
@@ -21,5 +26,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-    matcher: ['/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)'],
+    matcher: ['/((?!api|_next/static|_next/image|img|favicon.ico|sitemap.xml|robots.txt).*)'],
 };
