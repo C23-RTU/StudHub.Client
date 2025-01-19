62dd55eba66c207d807ee5525ade60aaf6a5e9b9
TankistPro <nakol.nayk@mail.ru>
2025-01-19 20:37:34 +0300

    Удален httpOnly

--- src/server-actions/middleware/authorization-protect.middleware.ts
+++ src/server-actions/middleware/authorization-protect.middleware.ts
@@ -18,7 +18,6 @@ export async function authorizationProtect(request: NextRequest) {
         response.cookies.set(EnumTokens.ACCESS_TOKEN, tokens.accessToken, { expires: Date.now() + 2592000000 }); // 30 дней
         response.cookies.set(EnumTokens.REFRESH_TOKEN, tokens.refreshToken, {
             expires: Date.now() + 2592000000,
-            httpOnly: true,
         }); // 30 дней
     }
 
