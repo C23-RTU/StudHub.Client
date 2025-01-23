a9c447971ac8cca15f02fac8998b06f81e886d39
Justifall <84130430+Angst04@users.noreply.github.com>
2025-01-23 22:16:43 +0300

    Верстка страницы поиска клубов

--- src/app/(auth)/clubs/Clubs.tsx
+++ src/app/(auth)/clubs/Clubs.tsx
@@ -1,4 +1,8 @@
+import { ClubCard } from '@/components/ClubCard/ClubCard';
+import { SearchInput } from '@/components/ui/SearchInput/SearchInput';
+
 import { Header, HeaderTitle } from '@/hoc/Header/Header';
+import { MainContent } from '@/hoc/MainContent/MainContent';
 
 export function Clubs() {
     return (
@@ -6,6 +10,38 @@ export function Clubs() {
             <Header>
                 <HeaderTitle>Поиск клубов</HeaderTitle>
             </Header>
+
+            <MainContent>
+                <div>
+                    <SearchInput placeholder="Поиск..." />
+                </div>
+                <p className="text-lg font-semibold">Рекомендуем</p>
+                <div className="flex flex-col gap-4">
+                    <ClubCard
+                        image="/img/test.jpg"
+                        name="Клуб хакатонщиков"
+                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vehicula eu"
+                    />
+                    <ClubCard
+                        image="/img/test.jpg"
+                        name="Клуб хакатонщиков"
+                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vehicula eu"
+                    />
+                </div>
+                <p className="text-lg font-semibold">Популярные</p>
+                <div className="flex flex-col gap-4">
+                    <ClubCard
+                        image="/img/test.jpg"
+                        name="Клуб хакатонщиков"
+                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vehicula eu"
+                    />
+                    <ClubCard
+                        image="/img/test.jpg"
+                        name="Клуб хакатонщиков"
+                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vehicula eu"
+                    />
+                </div>
+            </MainContent>
         </div>
     );
 }
---
+++ src/components/ClubCard/ClubCard.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import Image from 'next/image';
+
+interface Props {
+    image: string;
+    name: string;
+    description: string;
+}
+
+export function ClubCard({ image, name, description }: Props) {
+    return (
+        <article className="flex w-full hover:cursor-pointer" role="listitem">
+            <div>
+                <Image
+                    src={image}
+                    alt={`${name}'s avatar`}
+                    width={40}
+                    height={40}
+                    className="rounded-md w-14 h-14 min-w-[40px]"
+                />
+            </div>
+
+            <div className="flex flex-col ml-3 justify-between overflow-hidden">
+                <p
+                    className="text-m font-geologica font-semibold text-ellipsis overflow-hidden whitespace-nowrap"
+                    title={name}
+                >
+                    {name}
+                </p>
+                <p
+                    className="text-xs opacity-50 font-inter font-normal text-ellipsis overflow-hidden whitespace-nowrap"
+                    title={description}
+                >
+                    {description}
+                </p>
+            </div>
+
+            <div className="">
+                
+            </div>
+        </article>
+    );
+}
