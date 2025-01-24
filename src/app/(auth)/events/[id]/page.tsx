fa82ebc8cafdc446c34b14e8cfbe2f266ae79744
Konstantin Zhigaylo <zero@kostyazero.com>
2025-01-24 12:16:53 +0300

    feat(event): страница 404 и генерирование метаданных

--- src/app/(auth)/events/[id]/page.tsx
+++ src/app/(auth)/events/[id]/page.tsx
@@ -1,15 +1,29 @@
-import type { Metadata } from "next";
-import EventView from "./EventView";
-import { events } from "@/data/events";
-import type { ClubEvent } from "@/lib/types/event";
+import EventView from './EventView';
+import { events } from '@/data/events';
+import type { ClubEvent } from '@/lib/types/event';
+import EventNotFound from './EventNotFound';
 
-export const metadata: Metadata = {
-    title: 'Событие',
-    description: '',
-};
+export async function generateMetadata({ params }: { params: Promise<{ id: string }> }) {
+    const id = (await params).id;
+    const event: ClubEvent | undefined = events.find((e) => e.id === +id);
+
+    if (!event) {
+        return {
+            title: 'Событие не найдено',
+            description: '',
+        };
+    }
+
+    return {
+        title: `${event.title} - Событие`,
+        description: event.description,
+    };
+}
+
+export const dynamic = 'force-dynamic';
 
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
     const id = (await params).id;
     const event: ClubEvent | undefined = events.find((e) => e.id === +id);
-    return <EventView event={event || null} />;
+    return event ? <EventView event={event} /> : <EventNotFound />;
 }
