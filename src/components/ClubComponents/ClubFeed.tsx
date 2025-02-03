68c214e3f894459052b453bec8729d2c76ab0513
Justifall <84130430+Angst04@users.noreply.github.com>
2025-02-03 18:12:32 +0300

    Получение постов клуба

--- src/components/ClubComponents/ClubFeed.tsx
+++ src/components/ClubComponents/ClubFeed.tsx
@@ -1,14 +1,21 @@
-import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { useParams } from 'next/navigation';
 
 import { PostCard } from '../PostCard/PostCard';
 import { SearchInput } from '../ui/SearchInput/SearchInput';
 
 import { MainContent } from '@/hoc/MainContent/MainContent';
 import type { Post } from '@/lib/types/post';
+import { PostService } from '@/services/post.service';
 
 export function ClubFeed() {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const [posts, setPosts] = useState<Post[] | null>(null);
+    const { clubId } = useParams();
+
+    const { data: posts, isLoading } = useQuery({
+        queryKey: ['fetch-club-posts', clubId],
+        queryFn: () => PostService.getByClubId(Number(clubId)),
+        enabled: !isNaN(Number(clubId)),
+    });
 
     return (
         <MainContent>
@@ -16,8 +23,10 @@ export function ClubFeed() {
             <SearchInput placeholder="Поиск по постам..." />
 
             <div className="flex flex-col gap-10">
-                {posts ? (
-                    posts.map((post) => <PostCard key={post.id} post={post} />)
+                {isLoading ? (
+                    <p className="m-auto">Загрузка постов...</p>
+                ) : posts && posts.length > 0 ? (
+                    posts.map((post: Post) => <PostCard key={post.id} post={post} />)
                 ) : (
                     <p className="m-auto">Нет постов</p>
                 )}
