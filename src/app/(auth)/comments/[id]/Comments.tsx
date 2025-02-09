9a628a81b9123bb3801caa88fc42cf4924a705c1
Nikolay Gusev <58086757+TankistPro@users.noreply.github.com>
2025-02-09 16:27:42 +0300

    SN-55 Добавлен infinite scroll на страницу с комментариями (#29)
    * SN-55 Добавлен infinite scroll на страницу с комментариями
    
    * Рефакторинг комментариев
    
    * Убрал FIXME, корректная обработка последней страницы пагинации

--- src/app/(auth)/comments/[id]/Comments.tsx
+++ src/app/(auth)/comments/[id]/Comments.tsx
@@ -1,28 +1,27 @@
 'use client';
 
-import { useQuery } from '@tanstack/react-query';
 import { useRouter } from 'next/navigation';
 import { Fragment, useState } from 'react';
 
 import { CommentItem } from '@/components/CommentComponents/CommentItem';
+import { SkeletonCommentsList } from '@/components/CommentComponents/SkeletonCommentsList';
 import { TextareaEditorComment } from '@/components/CommentComponents/TextareaEditorComment';
 import { PostCard } from '@/components/PostCard/PostCard';
 import { BackButton } from '@/components/ui/BackButton/BackButton';
-import { Skeleton } from '@/components/ui/skeleton';
 
+import { useInfinityComments } from './useInfinityComments';
 import { Header, HeaderTitle } from '@/hoc/Header/Header';
 import { MainContent } from '@/hoc/MainContent/MainContent';
 import type { Post } from '@/lib/types/post';
-import { CommentService } from '@/services/comment.service';
 
 export function Comments({ post }: { post: Post }) {
     const router = useRouter();
     const [postState, updatePostState] = useState(post);
 
-    const { data, isLoading } = useQuery({
-        queryKey: ['fetch-post-comments', post.id],
-        queryFn: async () => await CommentService.getByPostId(post.id),
-    });
+    const {
+        ref,
+        infiniteQuery: { data, isLoading, isFetchingNextPage },
+    } = useInfinityComments(post.id);
 
     return (
         <div className="page pt-[90px]">
@@ -34,20 +33,22 @@ export function Comments({ post }: { post: Post }) {
             <MainContent>
                 <PostCard post={postState} />
                 <div className="flex flex-col gap-4 pb-[56px]">
-                    {!data && !isLoading && <p className="m-auto">Ошибка загрузки коментариев</p>}
-                    {data && data.length === 0 && <p className="m-auto">Комментариев нет</p>}
-                    {isLoading &&
-                        Array(3)
-                            .fill(0)
-                            .map((_, index) => <Skeleton key={index} className="h-[50px] w-full" />)}
+                    {data?.pages && data?.pages.length === 0 && <p className="m-auto">Комментариев нет</p>}
+                    {isLoading && <SkeletonCommentsList />}
 
                     {!isLoading &&
-                        data?.map((item, index) => (
-                            <Fragment key={item.id}>
-                                {index > 0 && <span className="h-[1px] bg-secondary w-3/5 mx-auto" />}
-                                <CommentItem comment={item} />
+                        data?.pages?.map((page, index) => (
+                            <Fragment key={index}>
+                                {page.map((item) => (
+                                    <Fragment key={item.id}>
+                                        {index > 0 && <span className="h-[1px] bg-secondary w-3/5 mx-auto" />}
+                                        <CommentItem comment={item} />
+                                    </Fragment>
+                                ))}
                             </Fragment>
                         ))}
+                    {isFetchingNextPage && <SkeletonCommentsList />}
+                    {!isFetchingNextPage && <div ref={ref} />}
 
                     <TextareaEditorComment postId={post.id} updatePost={updatePostState} />
                 </div>
---
+++ src/components/CommentComponents/SkeletonCommentItem.tsx
@@ -0,0 +1,5 @@
+import { Skeleton } from '../ui/skeleton';
+
+export function SkeletonCommentItem() {
+    return <Skeleton className="h-[50px] w-full" />;
+}
