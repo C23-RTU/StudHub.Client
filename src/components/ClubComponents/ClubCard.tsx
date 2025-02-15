8e6b05adc1262c70a8aaa8c97b8c816a3c2d5515
Justifall <84130430+Angst04@users.noreply.github.com>
2025-02-16 00:42:26 +0300

    Получение картинок для страницы поиска клубов

--- src/components/ClubComponents/ClubCard.tsx
+++ src/components/ClubComponents/ClubCard.tsx
@@ -2,18 +2,26 @@
 
 import Image from 'next/image';
 
+import { API_PATH } from '@/api/axios-client/base';
+
 import { SmallSubscribeButton } from './SmallSubscribeButton';
 
 interface Props {
-    image: string;
+    imageUrl: string | undefined | null;
     name: string;
-    description: string;
+    description: string | undefined | null;
 }
 
-export function ClubCard({ image, name, description }: Props) {
+export function ClubCard({ imageUrl, name, description }: Props) {
     return (
         <figure className="flex w-full hover:cursor-pointer items-center" role="listitem">
-            <Image src={image} alt={`${name}'s avatar`} width={32} height={32} className="rounded-lg w-12 h-12" />
+            <Image
+                src={imageUrl ? `${API_PATH}/${imageUrl}` : '/img/clubplaceholder.png'}
+                alt={`${name}'s avatar`}
+                width={32}
+                height={32}
+                className="rounded-lg w-12 h-12"
+            />
 
             <div className="flex flex-col flex-grow ml-3 mr-3 justify-between overflow-hidden">
                 <p
@@ -24,7 +32,7 @@ export function ClubCard({ image, name, description }: Props) {
                 </p>
                 <p
                     className="text-xs opacity-50 font-inter font-normal text-ellipsis overflow-hidden whitespace-nowrap"
-                    title={description}
+                    title={description || ''}
                 >
                     {description}
                 </p>
