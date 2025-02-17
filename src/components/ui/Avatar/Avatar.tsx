fcdf05e1adc1b172b83bf250c2ed57756f7cc813
TankistPro <nakol.nayk@mail.ru>
2025-02-18 01:02:39 +0300

    Починил динамические стили, при использовании tailwind CSS

--- src/components/ui/Avatar/Avatar.tsx
+++ src/components/ui/Avatar/Avatar.tsx
@@ -7,19 +7,24 @@ import { getStaticImg } from '@/lib/helpers/getStaticImg.helper';
 interface Props {
     src: string | null | undefined;
     loaderSize?: number;
-    size: number;
+    size?: number;
     alt?: string;
 }
 
-export function Avatar({ src, size, alt, loaderSize }: Props) {
+export function Avatar({ src, size = 40, alt, loaderSize }: Props) {
     return (
-        <div className={`overflow-hidden rounded-full h-[${size}px] w-[${size}px] shrink-0`}>
+        <div
+            className={`overflow-hidden rounded-full shrink-0`}
+            style={{
+                height: `${size}px`,
+                width: `${size}px`,
+            }}
+        >
             <LoaderImage
                 src={src ? getStaticImg(src) : '/img/avatar.png'}
                 loaderSize={loaderSize}
                 width={size}
                 height={size}
-                className={`m-h-[${size}px] m-w-[${size}px]`}
                 alt={alt || 'avatar'}
             />
         </div>
