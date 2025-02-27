2840224835e2792dc5d47941e53af097e75e2585
Konstantin Zhigaylo <zero@kostyazero.com>
2025-02-27 19:39:44 +0300

    refactor(skeletons): правки

--- src/components/Skeletons/SkeletonList.tsx
+++ src/components/Skeletons/SkeletonList.tsx
@@ -1,7 +1,13 @@
 import { SkeletonItem } from '@/components/Skeletons/SkeletonItem';
 
 export function SkeletonList() {
-    return Array(3)
-        .fill(0)
-        .map((_, index) => <SkeletonItem key={index} />);
+    return (
+        <div className="flex flex-col gap-4">
+            {Array(3)
+                .fill(0)
+                .map((_, index) => (
+                    <SkeletonItem key={index} />
+                ))}
+        </div>
+    );
 }
