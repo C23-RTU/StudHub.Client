0005ebd69713aaeea3ab03d1486f09ff5ee81a17
TankistPro <nakol.nayk@mail.ru>
2025-03-01 14:46:13 +0300

    Починил лайки

--- src/components/ui/PostActionButton/usePostReaction.tsx
+++ src/components/ui/PostActionButton/usePostReaction.tsx
@@ -1,16 +1,15 @@
-import { useMutation } from '@tanstack/react-query';
-
-import { queryClient } from '@/components/Provider/getQueryClient';
+import { useMutation, useQueryClient, type InfiniteData } from '@tanstack/react-query';
 
 import { reactionApi } from '@/api/api';
 import type { PostDetailDTO } from '@/api/axios-client/models';
 
 export const usePostReaction = (post: PostDetailDTO) => {
+    const queryClient = useQueryClient();
+
     const { mutate } = useMutation({
         mutationKey: ['toggle-reaction'],
         mutationFn: async () => await reactionApi.reactionsToggleReaction(post.id),
-        onError: async (error) => {
-            console.log(error);
+        onError: async () => {
             const { toast } = await import('react-hot-toast');
             toast.error('Не удалось выставить лайк');
         },
@@ -21,11 +20,22 @@ export const usePostReaction = (post: PostDetailDTO) => {
                 reactionCount: post.isUserReacted ? post.reactionCount - 1 : post.reactionCount + 1,
             };
 
+            queryClient.setQueryData(['fetch-post', post.id], () => newPost);
             queryClient.setQueryData(
                 ['fetch-feed-posts'],
-                (oldData: PostDetailDTO[] | null) => oldData && oldData.map((p) => (p.id === post.id ? newPost : p))
+                (oldData: InfiniteData<PostDetailDTO[], unknown>) => {
+                    if (!oldData) return;
+
+                    const pages = oldData.pages.map((page) => {
+                        return page.map((post) => (post.id === newPost.id ? newPost : post));
+                    });
+
+                    return {
+                        pageParams: oldData.pageParams,
+                        pages
+                    }
+                }
             );
-            queryClient.setQueryData(['fetch-post', post.id], () => newPost);
         },
     });
 
