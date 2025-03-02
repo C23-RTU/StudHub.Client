bb9461cc17cadcd6b038662a6e72d19841098532
TankistPro <nakol.nayk@mail.ru>
2025-03-03 00:03:31 +0300

    Исрпвлена ошибка подгрузки клубов в профиле

--- src/app/(auth)/(withMenuNavigation)/profile/Profile.tsx
+++ src/app/(auth)/(withMenuNavigation)/profile/Profile.tsx
@@ -1,78 +1,82 @@
 'use client';
 
-import { SettingBadge } from '@/components/Badge/SettingBadge/SettingBadge';
-import { Avatar } from '@/components/ui/Avatar/Avatar';
-import { Header, HeaderTitle } from '@/hoc/Header/Header';
-import { MainContent } from '@/hoc/MainContent/MainContent';
-import { ClubCard } from '@/components/ClubComponents/ClubCard';
+import { useQuery } from '@tanstack/react-query';
 import { IdCard, MessageSquare } from 'lucide-react';
 import Link from 'next/link';
+
+import { SettingBadge } from '@/components/Badge/SettingBadge/SettingBadge';
+import { ClubCard } from '@/components/ClubComponents/ClubCard';
+import { SkeletonList } from '@/components/Skeletons/SkeletonList';
+import { Avatar } from '@/components/ui/Avatar/Avatar';
+
 import { AUTH_PAGE } from '@/lib/config/routes.config';
-import { useQuery } from '@tanstack/react-query';
+
 import { userApi } from '@/api/api';
-import { Skeleton } from '@/components/ui/skeleton';
 import type { PersonDetailDTO } from '@/api/axios-client/models';
 
+import { Header, HeaderTitle } from '@/hoc/Header/Header';
+import { MainContent } from '@/hoc/MainContent/MainContent';
+
 type Props = {
-  user: PersonDetailDTO;
+    user: PersonDetailDTO;
 };
 
 export default function Profile({ user }: Props) {
-  const { data: clubs, isLoading: isLoadingClubs, error } = useQuery({
-    queryKey: ['fetch-user-clubs'],
-    queryFn: async () => (await userApi.userGetSubscribedClubs(0, 3)).data,
-  });
+    const {
+        data: userClubs,
+        isLoading,
+        error,
+    } = useQuery({
+        queryKey: ['fetch-profile-user-clubs'],
+        queryFn: async () => (await userApi.userGetSubscribedClubs(0, 3)).data,
+    });
 
-  const userClubs = clubs ? clubs : [];
+    return (
+        <div className="page">
+            <Header>
+                <HeaderTitle>Профиль</HeaderTitle>
+                <SettingBadge />
+            </Header>
 
-  return (
-    <div className="page">
-      <Header>
-        <HeaderTitle>Профиль</HeaderTitle>
-        <SettingBadge />
-      </Header>
-
-      <MainContent>
-        <div className="flex flex-row gap-4">
-          <Avatar src={user?.imagePath} size={80} alt={'Изображение профиля'} />
-          <div className="flex flex-col my-auto gap-0">
-            <p className="text-xl font-bold font-geologica max-w-[250px] overflow-hidden whitespace-nowrap text-ellipsis">
-              {user?.firstName} {user?.lastName}
-            </p>
-            <p className="text-sm text-neutral-400">был недавно</p>
-          </div>
+            <MainContent>
+                <div className="flex flex-row gap-4">
+                    <Avatar src={user?.imagePath} size={80} alt={'Изображение профиля'} />
+                    <div className="flex flex-col my-auto gap-0">
+                        <p className="text-xl font-bold font-geologica max-w-[250px] overflow-hidden whitespace-nowrap text-ellipsis">
+                            {user?.firstName} {user?.lastName}
+                        </p>
+                        <p className="text-sm text-neutral-400">был недавно</p>
+                    </div>
+                </div>
+                <div className="flex flex-col gap-4">
+                    <div className="flex flex-row gap-2">
+                        <MessageSquare size={20} />
+                        <p className="text-sm max-w-full overflow-hidden whitespace-nowrap text-ellipsis">
+                            {user?.about || '...'}
+                        </p>
+                    </div>
+                    <div className="flex flex-row gap-2">
+                        <IdCard size={20} />
+                        <p className="text-sm max-w-full overflow-hidden whitespace-nowrap text-ellipsis">
+                            {user?.institute?.name || 'Нет института'}
+                        </p>
+                    </div>
+                </div>
+                <div className="flex flex-col gap-4">
+                    <div className="flex flex-row justify-between">
+                        <p className="font-geologica font-bold text-lg">Подписки</p>
+                        <Link href={AUTH_PAGE.PROFILE_CLUBS} className="font-inter font-light text-primary">
+                            Показать все
+                        </Link>
+                    </div>
+                    {isLoading && <SkeletonList amount={3} />}
+                    {!isLoading &&
+                        userClubs &&
+                        userClubs.length > 0 &&
+                        userClubs.map((club) => <ClubCard key={club.id} club={club} />)}
+                    {error && <p className="text-neutral-400 text-center">Ошибка загрузки подписок</p>}
+                </div>
+            </MainContent>
         </div>
-        <div className="flex flex-col gap-4">
-          <div className="flex flex-row gap-2">
-            <MessageSquare size={20} />
-            <p className="text-sm max-w-full overflow-hidden whitespace-nowrap text-ellipsis">
-              {user?.about || '...'}
-            </p>
-          </div>
-          <div className="flex flex-row gap-2">
-            <IdCard size={20} />
-            <p className="text-sm max-w-full overflow-hidden whitespace-nowrap text-ellipsis">
-              {user?.institute?.name || "Нет института"}
-            </p>
-          </div>
-        </div>
-        <div className="flex flex-col gap-4">
-          <div className="flex flex-row justify-between">
-            <p className="font-geologica font-bold text-lg">Подписки</p>
-            <Link href={`${AUTH_PAGE.PROFILE_CLUBS}`} className="font-inter font-light text-primary">
-              Показать все
-            </Link>
-          </div>
-          {isLoadingClubs ? (
-            <Skeleton className="h-40 w-full" />
-          ) : userClubs.length === 0 ? (
-            <p className="text-neutral-400 text-center">Вы пока не подписаны ни на один клуб</p>
-          ) : (
-            userClubs.map((club) => <ClubCard key={club.id} club={club} />)
-          )}
-          {error && <p className="text-neutral-400 text-center">Ошибка загрузки подписок</p>}
-        </div>
-      </MainContent>
-    </div>
-  );
+    );
 }
--- src/app/(auth)/(withMenuNavigation)/profile/clubs/ProfileClubs.tsx
+++ src/app/(auth)/(withMenuNavigation)/profile/clubs/ProfileClubs.tsx
@@ -1,38 +1,42 @@
 'use client';
-import { ClubCard } from '@/components/ClubComponents/ClubCard';
-import { BackButton } from '@/components/ui/BackButton/BackButton';
 
-import { Header, HeaderTitle } from '@/hoc/Header/Header';
-import { userApi } from '@/api/api';
-import { SkeletonList } from '@/components/Skeletons/SkeletonList';
-import { useInfinityScroll } from '@/hooks/useInfinityScroll';
 import { useMemo } from 'react';
 
+import { ClubCard } from '@/components/ClubComponents/ClubCard';
+import { SkeletonList } from '@/components/Skeletons/SkeletonList';
+import { BackButton } from '@/components/ui/BackButton/BackButton';
+
+import { useInfinityScroll } from '@/hooks/useInfinityScroll';
+
+import { userApi } from '@/api/api';
+
+import { Header, HeaderTitle } from '@/hoc/Header/Header';
+
 export default function ProfileClubs() {
-    const { ref, infiniteQuery: { data: clubs, isLoading, isFetchingNextPage, hasNextPage } } = useInfinityScroll({
+    const {
+        ref,
+        infiniteQuery: { data: clubs, isLoading, isFetchingNextPage, hasNextPage },
+    } = useInfinityScroll({
         queryKey: ['fetch-user-clubs'],
         queryFn: async (page) => (await userApi.userGetSubscribedClubs(page.pageParam, 12)).data,
         pageSize: 12,
     });
 
-    const userClubs = useMemo(() => 
-        clubs ? clubs.pages.flatMap((page) => page) : []
-    , [clubs]);
+    const userClubs = useMemo(() => (clubs ? clubs.pages.flatMap((page) => page) : []), [clubs]);
 
     return (
         <div className="page">
-            <Header className='justify-start gap-4'>
+            <Header className="justify-start gap-4">
                 <BackButton />
                 <HeaderTitle>Подписки</HeaderTitle>
             </Header>
             <div className="space-y-4">
-                { isLoading && <SkeletonList /> }
-                {userClubs.length === 0 ? (
+                {(isFetchingNextPage || isLoading) && <SkeletonList />}
+                {!isLoading && userClubs.length === 0 ? (
                     <p className="text-center text-neutral-400">Вы пока не подписаны ни на один клуб</p>
                 ) : (
                     userClubs.map((club) => <ClubCard key={club.id} club={club} />)
                 )}
-                {isFetchingNextPage && <SkeletonList />}
                 {!hasNextPage && <p className="text-center text-neutral-400">На этом все!</p>}
                 <div ref={ref}></div>
             </div>
