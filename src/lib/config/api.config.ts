f95fed2a9a329945bd3b1a8f1d786ed2c8bb9ec3
TankistPro <nakol.nayk@mail.ru>
2025-07-13 22:52:38 +0300

    fix: изменен BASE_API_URL

--- src/lib/config/api.config.ts
+++ src/lib/config/api.config.ts
@@ -1,3 +1,3 @@
 import { isProduction } from '../helpers/isProduction.helper';
 
-export const BASE_API_URL = isProduction ? 'https://setka-rtu.ru/api' : 'https://localhost:3000/api';
+export const BASE_API_URL = isProduction ? 'http://api.setka-rtu.ru/api' : 'https://localhost:3000/api';
