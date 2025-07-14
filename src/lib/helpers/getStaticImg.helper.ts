f67e2667a6274c73c7bae253888a5a8ae6b418f1
TankistPro <nakol.nayk@mail.ru>
2025-07-14 17:33:34 +0300

    fix(getStaticImg): исправил получение картинок

--- src/lib/helpers/getStaticImg.helper.ts
+++ src/lib/helpers/getStaticImg.helper.ts
@@ -1,3 +1,5 @@
 // import { BASE_API_URL } from "../config/api.config";
 
-export function getStaticImg(imageUrl: string) { return  `https://setka-rtu.ru/api/${imageUrl}` }
+export function getStaticImg(imageUrl: string) {
+    return `https://api.setka-rtu.ru/api/${imageUrl}`;
+}
