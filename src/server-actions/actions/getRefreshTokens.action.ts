3adfd90e6277a1820c19b574eb4835a017864bef
TankistPro <nakol.nayk@mail.ru>
2025-08-13 21:31:31 +0300

    refact: переработана система авторизации, выставление auth-кук полностью делегировано на api

--- src/server-actions/actions/getRefreshTokens.action.ts
+++ src/server-actions/actions/getRefreshTokens.action.ts
@@ -1,12 +1,11 @@
-import type { TokenModel } from '@/api/axios-client';
-
+// import type { TokenModel } from '@/api/axios-client';
 import { SERVER_AXIOS } from '../server-axios';
 
 export async function getNewTokens(refreshToken: string, accessToken: string) {
-    const response = await SERVER_AXIOS.get<TokenModel>(`/auth/refreshTokens`, {
+    const response = await SERVER_AXIOS.get(`/auth/refreshTokens`, {
         headers: {
             Cookie: `refreshToken=${refreshToken};accessToken=${accessToken}`,
         },
     });
-    return response.data;
+    return response;
 }
