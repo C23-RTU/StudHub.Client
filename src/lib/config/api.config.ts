f3e180424ce2f273ec09b6ed0f9ff30aac2b990c
TankistPro <nakol.nayk@mail.ru>
2025-08-14 22:29:59 +0300

    feat: добавлен конфиг для генерация api-admin

--- src/lib/config/api.config.ts
+++ src/lib/config/api.config.ts
@@ -1,3 +1,4 @@
 import { isProduction } from '../helpers/isProduction.helper';
 
 export const BASE_API_URL = isProduction ? 'https://setka-rtu.ru/api' : 'https://localhost:3000/api';
+export const BASE_ADMIN_API_URL = isProduction ? 'https://setka-rtu.ru/admin-api' : 'https://localhost:3000/admin-api';
