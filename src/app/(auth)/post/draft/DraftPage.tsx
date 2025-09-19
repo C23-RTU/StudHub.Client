c9785e98b38352395a58a0decdf5b8093346b230
TankistPro <nakol.nayk@mail.ru>
2025-09-19 20:49:16 +0300

    feat

--- src/app/(auth)/post/draft/DraftPage.tsx
+++ src/app/(auth)/post/draft/DraftPage.tsx
@@ -5,7 +5,9 @@ import { SaveIcon } from 'lucide-react';
 import dynamic from 'next/dynamic';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
+import { type SubmitHandler, useForm } from 'react-hook-form';
 
+import { Page } from '@/components/Page';
 import { BackButton } from '@/components/ui/BackButton';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
@@ -17,38 +19,48 @@ const EditorDynamic = dynamic(() => import('@/components/ui/TextEditor/Editor').
     ssr: false,
 });
 
+interface IPostForm {
+    title: string;
+    content: object;
+}
+
 export function DraftPage() {
     const router = useRouter();
     const [data, setData] = useState<OutputData | undefined>();
 
-    console.log(data);
+    const { register, setValue, handleSubmit } = useForm<IPostForm>({
+        mode: 'onChange',
+    });
+
+    const submit: SubmitHandler<IPostForm> = (data) => {
+        console.log(data);
+    };
 
     return (
-        <div className="page mx-auto h-svh max-w-[1024px]">
+        <Page>
             <Header>
-                <div className="flex flex-row items-center gap-4">
+                <div className="flex w-full flex-row items-center gap-4">
                     <BackButton onClick={() => router.back()} />
                     <HeaderTitle>Создание</HeaderTitle>
                 </div>
-                <Button className="h-10 w-10">
-                    <SaveIcon />
-                </Button>
             </Header>
-            <MainContent className="mb-0">
-                <div className="bg-secondary flex h-20 w-full cursor-pointer items-center justify-center rounded">
-                    Image uploader
-                </div>
-
-                <Input
-                    className="active:none border-none bg-transparent px-0 !text-2xl font-bold placeholder:text-[#707684] focus:shadow-none focus:ring-0"
-                    placeholder="Заголовок поста..."
-                />
-                <EditorDynamic
-                    value={data}
-                    onChange={(payload) => setData(payload)}
-                    placeholder="Напиши свой изумительный пост..."
-                />
+            <MainContent className="mb-0 px-2">
+                <form onSubmit={handleSubmit(submit)} className="flex w-full flex-col">
+                    <Input
+                        className="active:none border-none bg-transparent px-0 !text-2xl font-bold placeholder:text-[#707684] focus:shadow-none focus:ring-0"
+                        placeholder="Заголовок поста..."
+                        {...register('title')}
+                    />
+                    <EditorDynamic
+                        value={data}
+                        onChange={(payload) => setValue('content', payload)}
+                        placeholder="Напиши свой изумительный пост..."
+                    />
+                    <Button>
+                        Опубликовать <SaveIcon />
+                    </Button>
+                </form>
             </MainContent>
-        </div>
+        </Page>
     );
 }
