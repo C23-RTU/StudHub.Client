416f6b0f69e89ed8cabaed62efbb813e1be39a76
Konstantin Zhigaylo <zero@kostyazero.com>
2025-09-28 19:48:34 +0300

    chore(gitignore): добавил bun.lock в .gitignore

--- src/components/MainLoader.tsx
+++ src/components/MainLoader.tsx
@@ -2,7 +2,7 @@ import Loader from './Loader';
 
 export function MainLoader() {
     return (
-        <div className="flex h-[98svh] items-center justify-center">
+        <div className="max-w-page flex h-[98svh] items-center justify-center">
             <figure className="flex flex-col items-center justify-center gap-2">
                 <Loader />
                 <small className="text-xs text-neutral-500">Загрузка</small>
