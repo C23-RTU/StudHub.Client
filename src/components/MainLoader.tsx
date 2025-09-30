055af4951f75c2c2c5843c9625e791035e4ea378
Konstantin Zhigaylo <zero@kostyazero.com>
2025-09-30 18:38:25 +0300

    chore(loading): убрал анимацию при загрузке страницы

--- src/components/MainLoader.tsx
+++ src/components/MainLoader.tsx
@@ -1,7 +1,3 @@
-'use client';
-
-import { motion } from 'framer-motion';
-
 import Loader from './Loader';
 
 export function MainLoader() {
@@ -9,14 +5,7 @@ export function MainLoader() {
         <div className="max-w-content border-border flex h-[98svh] w-full items-center justify-center border-x">
             <figure className="flex flex-col items-center justify-center gap-2">
                 <Loader />
-                <motion.small
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    transition={{ delay: 1.5, duration: 0.5, ease: 'easeOut' }}
-                    className="text-muted text-xs"
-                >
-                    Загрузка
-                </motion.small>
+                <small className="text-muted text-xs">Загрузка</small>
             </figure>
         </div>
     );
